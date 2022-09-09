393f40ab54a04c8fcfa3e565e74929c794b2cf19
TinMartinn <95651717+TinMartinn@users.noreply.github.com>
2022-09-08 22:19:55 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <header>UwUshop
+      <nav>
+        <ul>
+          <li>
+            INICIO
+          </li>
+          <li>
+            ADULTXS
+          </li>
+          <li>
+            NIÑXS
+          </li>
+          <li>
+            PEDIDOS
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default App;
